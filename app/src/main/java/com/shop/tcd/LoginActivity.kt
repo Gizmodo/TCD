c4ee0dab258d101e03e43b8bb2bd18c7fd86ef67
HP <smirnovsergeyterem@gmail.com>
2021-08-10 00:24:13 +0300

    Main screen added

---
+++ app/src/main/java/com/shop/tcd/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.shop.tcd
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.textfield.TextInputEditText
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<TextInputEditText>(R.id.edLogin)
+        val message = editText.text.toString()
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
--- app/src/main/java/com/shop/tcd/MainActivity.kt
+++ app/src/main/java/com/shop/tcd/MainActivity.kt
@@ -2,10 +2,19 @@ package com.shop.tcd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
     }
 }
