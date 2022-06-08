7773d902c19f8bb9eb41b84091d788fc9131d4ef
SS <smirnovsergeyterem@gmail.com>
2022-06-08 17:58:53 +0300

    Исправлен экран ожидания ответа от сервера
    Fix #2

--- app/src/main/java/com/shop/tcd/ui/catalog/CatalogViewModel.kt
+++ app/src/main/java/com/shop/tcd/ui/catalog/CatalogViewModel.kt
@@ -7,25 +7,26 @@ import androidx.lifecycle.ViewModel
 import com.shop.tcd.App
 import com.shop.tcd.core.di.*
 import com.shop.tcd.core.extension.NetworkResult
+import com.shop.tcd.core.utils.ShimmerState
 import com.shop.tcd.data.dto.nomenclature.NomenclatureItem
 import com.shop.tcd.data.remote.ShopRepository
 import com.shop.tcd.data.repository.Repository
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import timber.log.Timber
 import javax.inject.Inject
 
 class CatalogViewModel : ViewModel() {
-    /**
-     * Сотояния для UI
-     **/
     private var _errorMessage = MutableLiveData<String>()
     val errorMessage: LiveData<String> get() = _errorMessage
 
     private var _exceptionMessage = MutableLiveData<String>()
     val exceptionMessage: LiveData<String> get() = _exceptionMessage
 
-    private var _loading = MutableLiveData<Boolean>()
-    val loading: LiveData<Boolean> get() = _loading
+    private val _state: MutableStateFlow<ShimmerState> = MutableStateFlow(ShimmerState.Empty)
+    val state: StateFlow<ShimmerState> = _state.asStateFlow()
 
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         onException(throwable)
@@ -51,10 +52,14 @@ class CatalogViewModel : ViewModel() {
     @Inject
     lateinit var shopRepository: ShopRepository
 
+    fun cancelCurrentJob() {
+        job.cancel()
+    }
+
     fun loadNomenclatureFull() {
         job.cancel()
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
-            _loading.postValue(true)
+            _state.value = ShimmerState.Loading
             when (val response = shopRepository.getNomenclatureFull()) {
                 is NetworkResult.Error -> {
                     onError("${response.code} ${response.message}")
@@ -63,24 +68,27 @@ class CatalogViewModel : ViewModel() {
                     onException(response.e)
                 }
                 is NetworkResult.Success -> {
-                    val nomenclatureList =
-                        response.data.nomenclature as ArrayList<NomenclatureItem>
-                    nomenclatureList.forEach { item ->
+                    val itemsCount = "Сохранение в БД ${response.data.nomenclature.size} записей"
+                    _state.value = ShimmerState.State(itemsCount)
+                    val list = response.data.nomenclature as ArrayList<NomenclatureItem>
+                    list.forEach { item ->
                         item.plu = item.plu.replace(("[^\\d.]").toRegex(), "")
                     }
-                    Timber.d("Загружено объектов ${nomenclatureList.size}")
                     repository.deleteAllNomenclature()
-                    repository.insertNomenclature(nomenclatureList)
+                    val jobInsert = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+                        repository.insertNomenclature(list)
+                    }
+                    jobInsert.join()
                 }
             }
-            _loading.postValue(false)
+            _state.value = ShimmerState.Finishing
         }
     }
 
     fun loadNomenclatureRemainders() {
         job.cancel()
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
-            _loading.postValue(true)
+            _state.value = ShimmerState.Loading
             when (val response = shopRepository.getNomenclatureRemainders()) {
                 is NetworkResult.Error -> {
                     onError("${response.code} ${response.message}")
@@ -89,24 +97,27 @@ class CatalogViewModel : ViewModel() {
                     onException(response.e)
                 }
                 is NetworkResult.Success -> {
-                    val nomenclatureList =
-                        response.data.nomenclature as ArrayList<NomenclatureItem>
-                    nomenclatureList.forEach { item ->
+                    val itemsCount = "Сохранение в БД ${response.data.nomenclature.size} записей"
+                    _state.value = ShimmerState.State(itemsCount)
+                    val list = response.data.nomenclature as ArrayList<NomenclatureItem>
+                    list.forEach { item ->
                         item.plu = item.plu.replace(("[^\\d.]").toRegex(), "")
                     }
-                    Timber.d("Загружено объектов ${nomenclatureList.size}")
                     repository.deleteAllNomenclature()
-                    repository.insertNomenclature(nomenclatureList)
+                    val jobInsert = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+                        repository.insertNomenclature(list)
+                    }
+                    jobInsert.join()
                 }
             }
-            _loading.postValue(false)
+            _state.value = ShimmerState.Finishing
         }
     }
 
     fun loadNomenclatureByPeriod(period: String) {
         job.cancel()
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
-            _loading.postValue(true)
+            _state.value = ShimmerState.Loading
             when (val response = shopRepository.getNomenclatureByPeriod(period)) {
                 is NetworkResult.Error -> {
                     onError("${response.code} ${response.message}")
@@ -115,30 +126,33 @@ class CatalogViewModel : ViewModel() {
                     onException(response.e)
                 }
                 is NetworkResult.Success -> {
-                    val nomenclatureList =
-                        response.data.nomenclature as ArrayList<NomenclatureItem>
-                    nomenclatureList.forEach { item ->
+                    val itemsCount = "Сохранение в БД ${response.data.nomenclature.size} записей"
+                    _state.value = ShimmerState.State(itemsCount)
+                    val list = response.data.nomenclature as ArrayList<NomenclatureItem>
+                    list.forEach { item ->
                         item.plu = item.plu.replace(("[^\\d.]").toRegex(), "")
                     }
-                    Timber.d("Загружено объектов ${nomenclatureList.size}")
                     repository.deleteAllNomenclature()
-                    repository.insertNomenclature(nomenclatureList)
+                    val jobInsert = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+                        repository.insertNomenclature(list)
+                    }
+                    jobInsert.join()
                 }
             }
-            _loading.postValue(false)
+            _state.value = ShimmerState.Finishing
         }
     }
 
     private fun onError(message: String) {
         Timber.e(message)
         _errorMessage.postValue(message)
-        _loading.postValue(false)
+        _state.value = ShimmerState.Finishing
     }
 
     private fun onException(throwable: Throwable) {
         Timber.e(throwable)
         _exceptionMessage.postValue(throwable.message)
-        _loading.postValue(false)
+        _state.value = ShimmerState.Finishing
     }
 
     override fun onCleared() {
